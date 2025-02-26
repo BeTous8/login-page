6a3e52751b8f6af0d35b534100a68d544e870321
BeTous8 <159721847+BeTous8@users.noreply.github.com>
2025-02-25 21:04:18 -0600

    Initial commit

---
+++ src/projects.js
@@ -0,0 +1,61 @@
+import { Todo } from "./tasks";
+
+class Project {
+  constructor(name) {
+    this.id = Date.now();
+    this.name = name;
+    this.task = [];
+  }
+
+  addTask(task) {
+    this.task.push(task);
+  }
+
+  getTasks() {
+    return this.task;
+  }
+
+  deleteTask(index) {
+    this.task.splice(index, 1);
+  }
+}
+
+function projectManager() {
+  let projects = [];
+
+  function createProject(projectName) {
+    const newProject = new Project(projectName);
+    projects.push(newProject);
+    // saveToLocalStorage();
+    return newProject;
+  }
+
+  function getAllProjects() {
+    return projects;
+  }
+
+  function projectFinder(projectName) {
+    // loop through all the projects if one of them match the given one then return that project
+    return projects.find((item) => item.name === projectName);
+  }
+
+  function removeProject(projectName) {
+    projects = projects.filter((item) => item.name !== projectName);
+    // saveToLocalStorage();
+    return projects;
+  }
+
+  function clearProjects() {
+    projects = [];
+  }
+
+  return {
+    createProject,
+    getAllProjects,
+    projectFinder,
+    removeProject,
+    clearProjects,
+  };
+}
+
+export { Project, projectManager };
